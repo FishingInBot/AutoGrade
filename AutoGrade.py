47d540c426ffaf2c9c35a51de747bbce88e52458
FishingInBot1 <fishinginbot@gmail.com>
2023-09-26 20:12:33 -0500

    Formatting and Bug Fixes
    Fixed a lot of the formatting with paths.
    Also fixed out files getting looped and src transfers failing. Also catches all.zip files now.

--- AutoGrade.py
+++ AutoGrade.py
@@ -11,33 +11,40 @@ defaultLocation = "Desktop\Grading"
 IDEALocation = os.path.join(os.environ['USERPROFILE'], "IdeaProjects\Grading")
 #This should be the location where you want the src folder to be in.
 
+
 standardNaming = True
 #this means that folders in the submission folder have the standard naming scheme for canvas. This ought to look like "name_some-ID_some-ID_name-of-file"
 #the important part is the name is first, and is followed by a '_' character.
+
+#DON'T TOUCH
+srcLocation = os.path.join(IDEALocation,"src")
+outLocation = os.path.join(IDEALocation,"out")
 
 def setFileLocation():
     # Take some input as the where submissions are (probably want a default spot)
     fileLocation = input("Enter location of submission files (nothing for default): \n")
     if (fileLocation == ""):
-        fileLocation = os.path.join(os.path.join(os.environ['USERPROFILE']), defaultLocation)
+        fileLocation = os.path.join(os.environ['USERPROFILE'], defaultLocation)
 
     return fileLocation
 
 def unzip(location):
     #Unzip any submissions
     #TODO maybe combine ones with same name?
-    files = glob.glob(location+".*(\.zip)")
+    files = glob.glob(root_dir=location, pathname="*.zip")
     for file in files:
-        filename = file.replace(location,"")
-        filename = filename.replace(".zip", "")
-        with ZipFile(file) as zipper:
-            zipper.extractall(path=location+filename)
-        os.remove(file)
+        source = os.path.join(location, file)
+        filename = file.replace(".zip", "")
+        dest = os.path.join(location,filename)
+        with ZipFile(source) as zipper:
+            zipper.extractall(path=dest)
+        os.remove(source)
 
 def openSubmissions(location):
-    for dir, subdir, file in os.walk(IDEALocation):
-        if "src" in subdir:
-            shutil.rmtree(os.path.join(IDEALocation,"src"))
+    if os.path.isdir(srcLocation):
+        shutil.rmtree(srcLocation)
+    if os.path.isdir(outLocation):
+        shutil.rmtree(outLocation)
 
     submissions = next(os.walk(location))[1]
     for submission in submissions:
@@ -47,23 +54,25 @@ def openSubmissions(location):
         srcFolder = ""
 
         for dirpath, subdirs, files in os.walk(os.path.join(location,submission)):
-            for x in subdirs:
-                if "src" in x and "MACOSX" not in dirpath:
-                    srcFolder = os.path.join(dirpath, x)
+            for subdir in subdirs:
+                if "src" in subdir and "MACOSX" not in dirpath:
+                    srcFolder = os.path.join(dirpath, subdir)
+
         if srcFolder == "":
             print("Unable to find src folder in directory: " + submission)
             proceed = "yes"
-            os.mkdir(os.path.join(IDEALocation,"src"))
-
         else:
-            shutil.copytree(srcFolder, os.path.join(IDEALocation,os.path.basename(srcFolder)))
+            shutil.copytree(srcFolder, srcLocation)
             print("Now looking at submission: " + srcFolder.replace(location+"\\", "").replace("\\src", ""))
 
         while proceed != "yes":
             proceed = input("Type yes to continue: ")
         
         #Now to reset for next submission
-        shutil.rmtree(os.path.join(IDEALocation,"src"))
+        if os.path.isdir(srcLocation): 
+            shutil.rmtree(srcLocation)
+        if os.path.isdir(outLocation): 
+            shutil.rmtree(outLocation)
 
 def main():
     fileLocation = setFileLocation()
